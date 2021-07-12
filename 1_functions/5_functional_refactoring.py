70109dc1a2d9bc775ab6c67b49244454281fa1b4
konflic <konflic@mail.ru>
2021-07-12 19:42:40 +0300

    Версия 3.2.1

---
+++ 1_functions/5_functional_refactoring.py
@@ -0,0 +1,19 @@
+while True:
+    a = input("Input number a: ")
+    b = input("Input number b: ")
+
+    a = int(a)
+    b = int(b)
+
+    action = input("Input action [+, -, /, *]: ")
+
+    if action == "+":
+        print(a + b)
+    elif action == "-":
+        print(a - b)
+    elif action == "/":
+        print(a / b)
+    else:
+        print(a * b)
+
+    print("Done!")
---
+++ 3_mfr/some_more.py
@@ -0,0 +1,5 @@
+print(any([True, 1, ""]))
+
+print(all([True, 1, ""]))
+
+print(dict(zip([1, 2, 3], "abc")))
